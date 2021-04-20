2ceb74fd58c4ab029ed0e487b72197fd7bffd445
sp20-bse-088 <81982527+sp20-bse-088@users.noreply.github.com>
2021-04-20 23:59:10 +0500

    Add files via upload

---
+++ Lab 8/Runner.java
@@ -0,0 +1,15 @@
+
+public class Runner {
+
+
+    public static void main(String[] args) {
+        book b= new book("Chemistry", 400, 360);
+        b.display();
+        tape t= new tape("Alchemist", 350, 480);
+        t.display();
+
+
+    }
+
+
+}
